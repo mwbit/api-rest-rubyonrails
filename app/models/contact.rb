5d1c09083f0d1cf00d00a8d3c20f3c85b8452459
mwbitt <mwbitt@gmail.com>
2018-08-09 23:44:34 -0300

    first commit

---
+++ app/models/contact.rb
@@ -0,0 +1,15 @@
+class Contact < ApplicationRecord
+    belongs_to :kind, optional: true
+    has_many :phones 
+    has_one :address
+
+    accepts_nested_attributes_for :phones, allow_destroy: true
+    accepts_nested_attributes_for :address, update_only: true
+
+    # def as_json(options={})
+    #     super(
+    #         root: true,
+    #         include: :kind
+    #     )
+    # end
+end
