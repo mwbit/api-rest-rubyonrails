c9cd9a9ac521b786099de1f3327747023d0eb610
mwbitt <mwbitt@gmail.com>
2018-08-10 00:48:10 -0300

    links meta

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -5,6 +5,16 @@ class ContactSerializer < ActiveModel::Serializer
   has_many :phones 
   has_one :address
 
+  #to aditional information use meta
+  meta do
+    {author: "author name"}
+  end
+
+  #to add link information
+  link(:self) {contact_path(object.id)}
+  link(:kind) {kind_path(object.kind.id)}
+  
+
   def attributes(*args)
     h = super(*args)
     #pt-BR
