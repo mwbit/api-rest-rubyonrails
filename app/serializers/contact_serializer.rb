d780d297b745e8dcbd66451d3af751f084fb12a0
mwbitt <mwbitt@gmail.com>
2018-08-10 00:23:49 -0300

    serializers adapter

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,15 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
+  belongs_to :kind
+  has_many :phones 
+  has_one :address
+
   def attributes(*args)
     h = super(*args)
-    h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
+    #pt-BR
+    #h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
+    h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
     h 
   end
 
