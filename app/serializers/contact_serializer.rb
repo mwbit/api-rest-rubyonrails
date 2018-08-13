dc6a1b8fb47fe3966e971d93c05df10b68302f37
mwbitt <mwbitt@gmail.com>
2018-08-13 08:48:55 -0300

    add phohes controller

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,10 +2,12 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
   belongs_to :kind do
-   link(:related) { contact_kind_url(object.id) }
+    link(:related) { contact_kind_url(object.id) }
   end
 
-  has_many :phones 
+  has_many :phones do
+    link(:related) { contact_phones_url(object.id) }
+  end
   has_one :address
 
   #to aditional information use meta
