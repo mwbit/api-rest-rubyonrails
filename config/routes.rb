24df28d5137ac3a3c1ad4233579fcad1882a56ec
mwbitt <mwbitt@gmail.com>
2018-08-13 11:09:29 -0300

    version control using queryparam for contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,30 @@
 Rails.application.routes.draw do
   resources :kinds
-  resources :contacts do
-    resource :kind, only: [:show]
-    resource :kind, only: [:show],path: 'relationships/kind'
-    
-    resource :phones, only: [:show]
-    resource :phone,  only: [:update, :create, :destroy]
-    resource :phones, only: [:show],path: 'relationships/phones'
+  scope module: 'v1' do
+    resources :contacts, :constraints => lambda { |request|request.params[:version] == "1" } do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show],path: 'relationships/kind'
+      
+      resource :phones, only: [:show]
+      resource :phone,  only: [:update, :create, :destroy]
+      resource :phones, only: [:show],path: 'relationships/phones'
 
-    resource :address, only: [:show, :update, :create, :destroy]
-    resource :address, only: [:show, :update, :create, :destroy],path: 'relationships/address'
+      resource :address, only: [:show, :update, :create, :destroy]
+      resource :address, only: [:show, :update, :create, :destroy],path: 'relationships/address'
+    end
+  end
+  scope module: 'v2' do
+    resources :contacts, :constraints => lambda { |request|request.params[:version] == "2" } do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show],path: 'relationships/kind'
+      
+      resource :phones, only: [:show]
+      resource :phone,  only: [:update, :create, :destroy]
+      resource :phones, only: [:show],path: 'relationships/phones'
 
+      resource :address, only: [:show, :update, :create, :destroy]
+      resource :address, only: [:show, :update, :create, :destroy],path: 'relationships/address'
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
