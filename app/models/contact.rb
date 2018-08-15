f62533c436a7705da25b7eb3d90d36764d5c173b
mwbitt <mwbitt@gmail.com>
2018-08-15 10:45:42 -0300

    errorserializer

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,10 @@
 class Contact < ApplicationRecord
+    validates_presence_of :kind
+    validates_presence_of :address
+    
+    
+
+
     belongs_to :kind, optional: true
     has_many :phones 
     has_one :address
